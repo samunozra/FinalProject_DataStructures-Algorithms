5e5e9a496452b1462ca49284959845abfd6ff5e8
samunozra <157034686+samunozra@users.noreply.github.com>
2025-04-09 09:51:30 -0600

    Project setup

---
+++ BinarySearchTree.cs
@@ -0,0 +1,1 @@
+﻿internal class BinarySearchTree<Node> { }
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+     
+    }
+}
+internal class Hero
+{
+    internal string Name { get => _name; }
+    internal int Strength { get; set; }
+    internal int Agility { get; set; }
+    internal int Intelligence { get; set; }
+    internal int Health { get; set; }
+    internal Queue<string> Inventory { get; }
+    private string _name;
+
+    internal Hero() { }
+    internal Hero(string name)
+    { _name = name; }
+}
